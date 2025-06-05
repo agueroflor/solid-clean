f3413e9f58b369eee70f01380e76b4da6faa233d
Florencia <agueroflo13@gmail.com>
2025-06-05 15:59:11 -0300

    feat: functions

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,39 @@
+(() => {
+
+    function getMovieById( id: string ) {
+        console.log({ id });
+    }
+
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    function getActorById( id: string ) {
+        console.log({ id });
+    }
+    
+    interface Movie {
+        title: string;
+        description: string;
+        rating: number;
+        cast: string[];
+    }
+    
+    function createMovie( { title, description, rating, cast }: Movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor', fullName, birthdate);
+        return true;        
+    }
+
+})();
+
+
